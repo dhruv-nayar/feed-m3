1f6ebe76f64edfa57b3120151d7c7ece865f122d
Dhruv Nayar <nayardhruv0@gmail.com>
2019-07-14 17:48:55 -0700

    test js function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const https = require('https');
 
 const app = express();
 
@@ -9,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req,res) => {
 	var list = ["urbano", "outfittero", "item3"];
-	res.json(list);
+	res.json(callEdemamAPI("a","b"));
 	console.log('Sent list of items');
 });
 
@@ -22,3 +23,7 @@ const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
+
+function callEdemamAPI(requiredFoods, allergens){
+	return ["urbano", "outfittero", "item3"];
+}
