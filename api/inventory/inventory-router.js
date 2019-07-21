f9ea4926a31a5ed45670f1cbb1c69fd47c1af5f0
Dhruv Nayar <nayardhruv0@gmail.com>
2019-07-20 19:29:06 -0700

    added test endpoint for getting sample inventory list

--- api/inventory/inventory-router.js
+++ api/inventory/inventory-router.js
@@ -2,46 +2,16 @@ const express = require('express');
 var router = express.Router();
 var path = require('path');
 var scriptName = path.basename(__filename);
-const OktaJwtVerifier = require('@okta/jwt-verifier');
 
+module.exports = router;
 
-const oktaJwtVerifier = new OktaJwtVerifier({
-  issuer: 'https://dev-510263.okta.com/oauth2/default',
-  clientId: '0oay8z7zujX06wgZR356',
-  assertClaims: {
-    aud: 'api://default',
-  },
+router.get('/getList', (req,res) => {
+	var list = ["item1", "item2", "item3"];
+	res.json(list);
+	console.log('Sent list of items');
 });
 
-/**
- * A simple middleware that asserts valid access tokens and sends 401 responses
- * if the token is not present or fails validation.  If the token is valid its
- * contents are attached to req.jwt
- */
-function authenticationRequired(req, res, next) {
-  const authHeader = req.headers.authorization || '';
-  const match = authHeader.match(/Bearer (.+)/);
-
-  if (!match) {
-    return res.status(401).end();
-  }
-
-  const accessToken = match[1];
-
-  return oktaJwtVerifier.verifyAccessToken(accessToken)
-    .then((jwt) => {
-      req.jwt = jwt;
-      next();
-    })
-    .catch((err) => {
-      res.status(401).send(err.message);
-    });
-}
-
-const app = express();
-
 router.get('/getTestInventory', function(req, res){
-
   //item name, age (days), category, storage type (fridge, freezer, pantry etc)
   var genericList = [
        {'name':'eggs', 'age':'14', 'category':'produce', 'storage':'fridge'},
@@ -52,4 +22,23 @@ router.get('/getTestInventory', function(req, res){
   res.json(genericList);
 });
 
-module.exports = router();
+// const express = require('express');
+// var router = express.Router();
+// var path = require('path');
+// var scriptName = path.basename(__filename);
+//
+//
+//
+// router.get('/getTestInventory', function(req, res){
+//
+//   //item name, age (days), category, storage type (fridge, freezer, pantry etc)
+//   var genericList = [
+//        {'name':'eggs', 'age':'14', 'category':'produce', 'storage':'fridge'},
+//       {'name':'broccoli', 'age':'14', 'category':'produce', 'storage':'fridge'},
+//       {'name':'cheese', 'age':'14', 'category':'produce', 'storage':'pantry'}
+//     ];
+//
+//   res.json(genericList);
+// });
+//
+// module.exports = router();
